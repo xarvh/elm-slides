3d7857b7fc3f969afeb97a795af2bab838aa42f3
xarvh <francesco.orsenigo@gmail.com>
2020-01-26 21:30:22 +0100

    elm-format example

--- examples/Main.elm
+++ examples/Main.elm
@@ -1,122 +1,110 @@
-import Slides exposing (md, mdFragments, slidesDefaultOptions)
-import Slides.Styles
-import Slides.SlideAnimation
-import List
+module Main exposing (main)
 
 import Css exposing (..)
 import Css.Global exposing (img)
+import List
+import Slides exposing (md, mdFragments, slidesDefaultOptions)
+import Slides.SlideAnimation
+import Slides.Styles
 
 
-main = Slides.app
+main =
+    Slides.app
+        { slidesDefaultOptions
+            | slideAnimator =
+                Slides.SlideAnimation.scroll
+            , style =
+                let
+                    imgWidthSnippet =
+                        img [ maxWidth (px 500) ]
 
-    { slidesDefaultOptions
-        | style = List.append
-             [ img [ maxWidth (px 500) ]
-             ]
-             <| Slides.Styles.elmMinimalist (hex "#fff") (hex "#ccc") (px 16) (hex "#000")
-        , slideAnimator = Slides.SlideAnimation.scroll
-    }
+                    baseStyleSnippets =
+                        Slides.Styles.elmMinimalist (hex "#fff") (hex "#ccc") (px 16) (hex "#000")
+                in
+                imgWidthSnippet :: baseStyleSnippets
+        }
+        [ md
+            """
+            # Elm
 
-    [ md
-        """
-        # Elm
+            _Or: making functional programming accessible_
 
-        _Or: making functional programming accessible_
+            by Francesco Orsenigo @xarvh
 
-        by Francesco Orsenigo @xarvh
+            """
+        , mdFragments
+            [ """
+              I love the concepts behind functional programming.
+              """
+            , """
+              So I tried to learn Haskell and Clojure.
+              """
+            , """
+              They are great languages, but I struggled every time
+              I wanted to go past a tutorial.
+              """
+            ]
+        , md
+            """
+            I tried Elm and I was happily reimplementing my own projects
+            in Elm shortly afterwards.
+            """
+        , md
+            """
+            How can we improve other FP languages?
 
-        """
-
-
-    , mdFragments
-        [   """
-            I love the concepts behind functional programming.
+            What is Elm doing right?
+            """
+        , md
             """
 
-        ,   """
-            So I tried to learn Haskell and Clojure.
+            # Easy app setup
+
+            ```elm
+            $ elm install elm-lang/html
+            ```
+
+            And we're ready to go!
+
+            """
+        , md
             """
 
-        ,   """
-            They are great languages, but I struggled every time
-            I wanted to go past a tutorial.
+            # Single-step build
+
+            ```elm
+            $ elm make HelloWorld.elm
+            ```
+
+            ➡
+
+            ```elm
+            index.html
+            ```
+            """
+        , md
+            """
+            # Accessible docs
+
+            Inline docs are displayed on http://package.elm-lang.org/
+
+            elm-package will refuse to publish anything without docs!
+            """
+        , md
+            """
+            # Official libraries for most common tasks
+
+            * Virtual DOM
+            * SVG rendering
+            * Markdown
+            * Http
+            * Geolocation
+            * Websockets
+            """
+        , md
+            """
+
+            @xarvh
+
             """
         ]
-
-
-    , md
-        """
-        I tried Elm and I was happily reimplementing my own projects
-        in Elm shortly afterwards.
-        """
-
-
-    , md
-        """
-        How can we improve other FP languages?
-
-        What is Elm doing right?
-        """
-
-
-    , md
-        """
-
-        # Easy app setup
-
-        ```elm
-        $ elm install elm-lang/html
-        ```
-
-        And we're ready to go!
-
-        """
-
-
-    , md
-        """
-
-        # Single-step build
-
-        ```elm
-        $ elm make HelloWorld.elm
-        ```
-
-        ➡
-
-        ```elm
-        index.html
-        ```
-        """
-
-
-    , md
-        """
-        # Accessible docs
-
-        Inline docs are displayed on http://package.elm-lang.org/
-
-        elm-package will refuse to publish anything without docs!
-        """
-
-
-    , md
-        """
-        # Official libraries for most common tasks
-
-        * Virtual DOM
-        * SVG rendering
-        * Markdown
-        * Http
-        * Geolocation
-        * Websockets
-        """
-
-
-    , md
-        """
-
-        @xarvh
-
-        """
-    ]
